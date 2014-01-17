7a3185593c59b46ad5d696c127a7ddf7e726e41c
Lighton Phiri <lighton.phiri@gmail.com>
2014-01-17 12:41:40 +0200

    [Configuration files] Modified Harvest.sh script
    Changed class paths in Harvest.sh script

--- installation/Harvest.sh
+++ installation/Harvest.sh
@@ -2,5 +2,5 @@
 cd /var/lib/tomcat7/webapps/harvester
 java -Dhttp.proxyHost=localhost -Dhttp.proxyPort=3128 -Xms512m -Xmx512m -cp WEB-INF/classes:WEB-INF/lib/mysql-connector-java.jar OAIHarvest $1
 
-java -cp portal/WEB-INF/classes:portal/WEB-INF/lib/mysql-connector-java.jar:portal/WEB-INF/lib/lucene-analyzers.jar:portal/WEB-INF/lib/lucene-core.jar  HarvestingMain
+java -cp ../portal/WEB-INF/classes:../portal/WEB-INF/lib/mysql-connector-java.jar:../portal/WEB-INF/lib/lucene-analyzers.jar:../portal/WEB-INF/lib/lucene-core.jar  HarvestingMain
 
