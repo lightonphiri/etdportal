46538a12b18fe96422f4c921a78d512fdbb0f1d5
Lighton Phir <lighton.phiri@gmail.com>
2016-09-30 00:54:04 +0200

    [MySQL database] Invalid default value for
    Used SET to ignore invalid date and timestamp
    default zero values: MySQL NO_ZERO_DATE

--- harvester/WEB-INF/db/create_db.sql
+++ harvester/WEB-INF/db/create_db.sql
@@ -1,5 +1,6 @@
 SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
+SET SQL_MODE='ALLOW_INVALID_DATES';
 
 DROP TABLE IF EXISTS Archive;
 DROP TABLE IF EXISTS Counter;
